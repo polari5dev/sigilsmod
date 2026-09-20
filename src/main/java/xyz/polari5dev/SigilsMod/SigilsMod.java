b5a04189bb1e8412d9d77c5ea02be06fd1718da9
polari5dev <polari5dev@proton.me>
2026-09-21 00:53:15 +0200

    feat(sigilcrafting): create recipes for speed, vitality, luck, haste

--- src/main/java/xyz/polari5dev/SigilsMod/SigilsMod.java
+++ src/main/java/xyz/polari5dev/SigilsMod/SigilsMod.java
@@ -27,6 +27,8 @@ public class SigilsMod extends JavaPlugin {
     }
 
     private void registerRecipes() {
+
+        // strength
         NamespacedKey key = new NamespacedKey(this, "strength_sigil");
 
         ShapedRecipe recipe = new ShapedRecipe(key, Sigils.create("strength"));
@@ -41,6 +43,74 @@ public class SigilsMod extends JavaPlugin {
         recipe.setIngredient('N', Material.NETHERITE_INGOT);
         recipe.setIngredient('S', Material.NETHER_STAR);
         recipe.setIngredient('b', Material.BLAZE_POWDER);
+        getServer().addRecipe(recipe);
+
+        // speed
+        key = new NamespacedKey(this, "speed_sigil");
+
+        recipe = new ShapedRecipe(key, Sigils.create("speed"));
+
+        recipe.shape(
+                "SsS",
+                "N D",
+                "SES");
+
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('E', Material.ENDER_EYE);
+        recipe.setIngredient('N', Material.NETHERITE_INGOT);
+        recipe.setIngredient('S', Material.NETHER_STAR);
+        recipe.setIngredient('s', Material.SUGAR);
+        getServer().addRecipe(recipe);
+
+        // vitality
+        key = new NamespacedKey(this, "vitality_sigil");
+
+        recipe = new ShapedRecipe(key, Sigils.create("vitality"));
+
+        recipe.shape(
+                "SgS",
+                "N D",
+                "SES");
+
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('E', Material.ENDER_EYE);
+        recipe.setIngredient('N', Material.NETHERITE_INGOT);
+        recipe.setIngredient('S', Material.NETHER_STAR);
+        recipe.setIngredient('g', Material.ENCHANTED_GOLDEN_APPLE);
+        getServer().addRecipe(recipe);
+
+        // luck
+        key = new NamespacedKey(this, "luck_sigil");
+
+        recipe = new ShapedRecipe(key, Sigils.create("luck"));
+
+        recipe.shape(
+                "SbS",
+                "N D",
+                "SES");
+
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('E', Material.ENDER_EYE);
+        recipe.setIngredient('N', Material.NETHERITE_INGOT);
+        recipe.setIngredient('S', Material.NETHER_STAR);
+        recipe.setIngredient('b', Material.EXPERIENCE_BOTTLE);
+        getServer().addRecipe(recipe);
+
+        // haste
+        key = new NamespacedKey(this, "haste_sigil");
+
+        recipe = new ShapedRecipe(key, Sigils.create("haste"));
+
+        recipe.shape(
+                "SpS",
+                "N D",
+                "SES");
+
+        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+        recipe.setIngredient('E', Material.ENDER_EYE);
+        recipe.setIngredient('N', Material.NETHERITE_INGOT);
+        recipe.setIngredient('S', Material.NETHER_STAR);
+        recipe.setIngredient('p', Material.NETHERITE_PICKAXE);
         getServer().addRecipe(recipe);
     }
 
