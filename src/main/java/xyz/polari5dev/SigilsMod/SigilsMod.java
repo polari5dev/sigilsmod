5791fca1582cd62b88884843916b0aa868da4652
polari5dev <polari5dev@proton.me>
2026-09-21 08:07:41 +0200

    chore(gitignore): changed .gitignore file to ignore build/, .gradle/.

--- src/main/java/xyz/polari5dev/SigilsMod/SigilsMod.java
+++ src/main/java/xyz/polari5dev/SigilsMod/SigilsMod.java
@@ -11,112 +11,112 @@ import xyz.polari5dev.SigilsMod.listeners.PlayerListener;
 
 public class SigilsMod extends JavaPlugin {
 
-    @Override
-    public void onEnable() {
+        @Override
+        public void onEnable() {
 
-        // Initialize managers
-        PluginManager.getInstance().initialize();
+                // Initialize managers
+                PluginManager.getInstance().initialize();
 
-        // Register listeners
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+                // Register listeners
+                getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
 
-        getLogger().info(getDescription().getName() + " has been enabled! Hello, User!");
+                getLogger().info(getDescription().getName() + " has been enabled! Hello, User!");
 
-        this.registerCommand("sigil", new SigilCommand());
-        registerRecipes();
-    }
+                this.registerCommand("sigil", new SigilCommand());
+                registerRecipes();
+        }
 
-    private void registerRecipes() {
+        private void registerRecipes() {
 
-        // strength
-        NamespacedKey key = new NamespacedKey(this, "strength_sigil");
+                // strength
+                NamespacedKey key = new NamespacedKey(this, "strength_sigil");
 
-        ShapedRecipe recipe = new ShapedRecipe(key, Sigils.create("strength"));
+                ShapedRecipe recipe = new ShapedRecipe(key, Sigils.create("strength"));
 
-        recipe.shape(
-                "SbS",
-                "N D",
-                "SES");
+                recipe.shape(
+                                "SbS",
+                                "N D",
+                                "SES");
 
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.ENDER_EYE);
-        recipe.setIngredient('N', Material.NETHERITE_INGOT);
-        recipe.setIngredient('S', Material.NETHER_STAR);
-        recipe.setIngredient('b', Material.BLAZE_POWDER);
-        getServer().addRecipe(recipe);
+                recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+                recipe.setIngredient('E', Material.ENDER_EYE);
+                recipe.setIngredient('N', Material.NETHERITE_INGOT);
+                recipe.setIngredient('S', Material.NETHER_STAR);
+                recipe.setIngredient('b', Material.BLAZE_POWDER);
+                getServer().addRecipe(recipe);
 
-        // speed
-        key = new NamespacedKey(this, "speed_sigil");
+                // speed
+                key = new NamespacedKey(this, "speed_sigil");
 
-        recipe = new ShapedRecipe(key, Sigils.create("speed"));
+                recipe = new ShapedRecipe(key, Sigils.create("speed"));
 
-        recipe.shape(
-                "SsS",
-                "N D",
-                "SES");
+                recipe.shape(
+                                "SsS",
+                                "N D",
+                                "SES");
 
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.ENDER_EYE);
-        recipe.setIngredient('N', Material.NETHERITE_INGOT);
-        recipe.setIngredient('S', Material.NETHER_STAR);
-        recipe.setIngredient('s', Material.SUGAR);
-        getServer().addRecipe(recipe);
+                recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+                recipe.setIngredient('E', Material.ENDER_EYE);
+                recipe.setIngredient('N', Material.NETHERITE_INGOT);
+                recipe.setIngredient('S', Material.NETHER_STAR);
+                recipe.setIngredient('s', Material.SUGAR);
+                getServer().addRecipe(recipe);
 
-        // vitality
-        key = new NamespacedKey(this, "vitality_sigil");
+                // vitality
+                key = new NamespacedKey(this, "vitality_sigil");
 
-        recipe = new ShapedRecipe(key, Sigils.create("vitality"));
+                recipe = new ShapedRecipe(key, Sigils.create("vitality"));
 
-        recipe.shape(
-                "SgS",
-                "N D",
-                "SES");
+                recipe.shape(
+                                "SgS",
+                                "N D",
+                                "SES");
 
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.ENDER_EYE);
-        recipe.setIngredient('N', Material.NETHERITE_INGOT);
-        recipe.setIngredient('S', Material.NETHER_STAR);
-        recipe.setIngredient('g', Material.ENCHANTED_GOLDEN_APPLE);
-        getServer().addRecipe(recipe);
+                recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+                recipe.setIngredient('E', Material.ENDER_EYE);
+                recipe.setIngredient('N', Material.NETHERITE_INGOT);
+                recipe.setIngredient('S', Material.NETHER_STAR);
+                recipe.setIngredient('g', Material.ENCHANTED_GOLDEN_APPLE);
+                getServer().addRecipe(recipe);
 
-        // luck
-        key = new NamespacedKey(this, "luck_sigil");
+                // luck
+                key = new NamespacedKey(this, "luck_sigil");
 
-        recipe = new ShapedRecipe(key, Sigils.create("luck"));
+                recipe = new ShapedRecipe(key, Sigils.create("luck"));
 
-        recipe.shape(
-                "SbS",
-                "N D",
-                "SES");
+                recipe.shape(
+                                "SbS",
+                                "N D",
+                                "SES");
 
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.ENDER_EYE);
-        recipe.setIngredient('N', Material.NETHERITE_INGOT);
-        recipe.setIngredient('S', Material.NETHER_STAR);
-        recipe.setIngredient('b', Material.EXPERIENCE_BOTTLE);
-        getServer().addRecipe(recipe);
+                recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+                recipe.setIngredient('E', Material.ENDER_EYE);
+                recipe.setIngredient('N', Material.NETHERITE_INGOT);
+                recipe.setIngredient('S', Material.NETHER_STAR);
+                recipe.setIngredient('b', Material.EXPERIENCE_BOTTLE);
+                getServer().addRecipe(recipe);
 
-        // haste
-        key = new NamespacedKey(this, "haste_sigil");
+                // haste
+                key = new NamespacedKey(this, "haste_sigil");
 
-        recipe = new ShapedRecipe(key, Sigils.create("haste"));
+                recipe = new ShapedRecipe(key, Sigils.create("haste"));
 
-        recipe.shape(
-                "SpS",
-                "N D",
-                "SES");
+                recipe.shape(
+                                "SpS",
+                                "N D",
+                                "SES");
 
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.ENDER_EYE);
-        recipe.setIngredient('N', Material.NETHERITE_INGOT);
-        recipe.setIngredient('S', Material.NETHER_STAR);
-        recipe.setIngredient('p', Material.NETHERITE_PICKAXE);
-        getServer().addRecipe(recipe);
-    }
+                recipe.setIngredient('D', Material.DIAMOND_BLOCK);
+                recipe.setIngredient('E', Material.ENDER_EYE);
+                recipe.setIngredient('N', Material.NETHERITE_INGOT);
+                recipe.setIngredient('S', Material.NETHER_STAR);
+                recipe.setIngredient('p', Material.NETHERITE_PICKAXE);
+                getServer().addRecipe(recipe);
+        }
 
-    @Override
-    public void onDisable() {
-        getLogger().info(getDescription().getName() + " has been disabled! Goodbye!");
-    }
+        @Override
+        public void onDisable() {
+                getLogger().info(getDescription().getName() + " has been disabled! Goodbye!");
+        }
 
 }
